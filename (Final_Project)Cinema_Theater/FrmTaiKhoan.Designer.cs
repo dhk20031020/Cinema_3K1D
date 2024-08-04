65bfb2dffd8f71f614a58166b57274c6f51fdb32
P1emon <khang113324@gmail.com>
2024-08-04 23:59:22 +0700

    Tạo form để xem - tạo - chỉnh - xóa tài khoản

---
+++ (Final_Project)Cinema_Theater/FrmTaiKhoan.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace _Final_Project_Cinema_Theater
+{
+    partial class FrmTaiKhoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LblHoTen = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.RbNV = new System.Windows.Forms.RadioButton();
+            this.RbAdmin = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.LblHoTen);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(459, 80);
+            this.panel1.TabIndex = 17;
+            // 
+            // LblHoTen
+            // 
+            this.LblHoTen.AutoSize = true;
+            this.LblHoTen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.LblHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblHoTen.Location = new System.Drawing.Point(94, 9);
+            this.LblHoTen.Name = "LblHoTen";
+            this.LblHoTen.Size = new System.Drawing.Size(255, 29);
+            this.LblHoTen.TabIndex = 8;
+            this.LblHoTen.Text = "Tài Khoản Nhân viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Username";
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(169, 184);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(233, 22);
+            this.TxtPassword.TabIndex = 14;
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(169, 117);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(233, 22);
+            this.TxtUsername.TabIndex = 13;
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnUpdate.Location = new System.Drawing.Point(327, 261);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 55);
+            this.BtnUpdate.TabIndex = 19;
+            this.BtnUpdate.Text = "Sửa";
+            this.BtnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.BackColor = System.Drawing.Color.Lime;
+            this.BtnAdd.Location = new System.Drawing.Point(226, 261);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 55);
+            this.BtnAdd.TabIndex = 18;
+            this.BtnAdd.Text = "Thêm";
+            this.BtnAdd.UseVisualStyleBackColor = false;
+            // 
+            // RbNV
+            // 
+            this.RbNV.AutoSize = true;
+            this.RbNV.Checked = true;
+            this.RbNV.Location = new System.Drawing.Point(25, 71);
+            this.RbNV.Name = "RbNV";
+            this.RbNV.Size = new System.Drawing.Size(113, 24);
+            this.RbNV.TabIndex = 11;
+            this.RbNV.TabStop = true;
+            this.RbNV.Text = "Nhân viên";
+            this.RbNV.UseVisualStyleBackColor = true;
+            // 
+            // RbAdmin
+            // 
+            this.RbAdmin.AutoSize = true;
+            this.RbAdmin.Location = new System.Drawing.Point(25, 38);
+            this.RbAdmin.Name = "RbAdmin";
+            this.RbAdmin.Size = new System.Drawing.Size(82, 24);
+            this.RbAdmin.TabIndex = 10;
+            this.RbAdmin.TabStop = true;
+            this.RbAdmin.Text = "Admin";
+            this.RbAdmin.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Cyan;
+            this.groupBox1.Controls.Add(this.RbNV);
+            this.groupBox1.Controls.Add(this.RbAdmin);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(26, 232);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 112);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loại Tài Khoản";
+            // 
+            // FrmTaiKhoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(459, 372);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUsername);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmTaiKhoan";
+            this.Text = "FrmTaiKhoan";
+            this.Load += new System.EventHandler(this.FrmTaiKhoan_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LblHoTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.RadioButton RbNV;
+        private System.Windows.Forms.RadioButton RbAdmin;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ (Final_Project)Cinema_Theater/FrmTaiKhoan.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Final_Project_Cinema_Theater
+{
+    public partial class FrmTaiKhoan : Form
+    {
+        public FrmTaiKhoan()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTaiKhoan_Load(object sender, EventArgs e)
+        {
+            //Nhận biến idNV của form FrmInfoNhanVien và hiển thị thông tin lên các label
+            LblHoTen.Text = $"Thông tin của {FrmInfoNhanVien.nameNV}\r\n ID Nhân Viên {FrmInfoNhanVien.idNV}";
+            //Kiểm tra idNV đã có tài khoản chưa
+            SQLCONNECTION mycon = new SQLCONNECTION();
+            mycon.conn.Open();
+            string sql = "SELECT COUNT(*) FROM TaiKhoan WHERE idNV = @idNV";
+            mycon.cmd = new SqlCommand(sql, mycon.conn);
+            mycon.cmd.Parameters.AddWithValue("@idNV", FrmInfoNhanVien.idNV);
+            int count = Convert.ToInt32(mycon.cmd.ExecuteScalar());
+            //Nếu đã có tài khoản thì sẽ hiện thông tin ra các textbox
+            if (count > 0)
+            {
+                string sqlSelect = "SELECT * FROM TaiKhoan WHERE idNV = @idNV";
+                mycon.cmd = new SqlCommand(sqlSelect, mycon.conn);
+                mycon.cmd.Parameters.AddWithValue("@idNV", FrmInfoNhanVien.idNV);
+                SqlDataReader dta = mycon.cmd.ExecuteReader();
+                if (dta.Read() == true)
+                {
+                    TxtUsername.Text = dta["UserName"].ToString();
+                    TxtPassword.Text = dta["Pass"].ToString();
+                    if (Convert.ToInt32(dta["LoaiTK"]) == 1)
+                    {
+                        RbAdmin.Checked = true;
+                    }
+                    else if (Convert.ToInt32(dta["LoaiTK"]) == 2)
+                    {
+                        RbNV.Checked = true;
+                    }
+                }
+                BtnAdd.Visible = false;
+            }
+            else
+            {
+                BtnUpdate.Visible = false;
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Lưu thông tin vào trong bảng TaiKhoan
+            SQLCONNECTION mycon = new SQLCONNECTION();
+            mycon.conn.Open();
+            // Lúc chọn radio button có giá trị là Admin thì sẽ lưu 1, Nhân viên thì giá trị lưu sẽ là 2
+            int loaiTK = 0;
+            if (RbAdmin.Checked == true)
+            {
+                loaiTK = 1;
+            }
+            else if (RbNV.Checked == true)
+            {
+                loaiTK = 2;
+            }
+            // Đảm bảo rằng giá trị idNV là một chuỗi ký tự trước khi sử dụng nó trong câu lệnh SQL
+            //Kiểm tra các thông tin đã được nhập hết chưa và có bị trùng idNV không
+            if (TxtUsername.Text == "" || TxtPassword.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                // Kiểm tra trùng id
+                string sqlCheckDuplicateId = "SELECT COUNT(*) FROM TaiKhoan WHERE idNV = @idNV";
+                mycon.cmd = new SqlCommand(sqlCheckDuplicateId, mycon.conn);
+                mycon.cmd.Parameters.AddWithValue("@idNV", FrmInfoNhanVien.idNV);
+
+                int count = Convert.ToInt32(mycon.cmd.ExecuteScalar());
+                if (count > 0)
+                {
+                    MessageBox.Show("ID đã tồn tại trong cơ sở dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    // Thêm tài khoản mới vào bảng TaiKhoan
+                    string sqlInsert = "INSERT INTO TaiKhoan (UserName, Pass, LoaiTK, idNV) VALUES (@UserName, @Pass, @LoaiTK, @idNV)";
+                    mycon.cmd = new SqlCommand(sqlInsert, mycon.conn);
+                    mycon.cmd.Parameters.AddWithValue("@UserName", TxtUsername.Text);
+                    mycon.cmd.Parameters.AddWithValue("@Pass", TxtPassword.Text);
+                    mycon.cmd.Parameters.AddWithValue("@LoaiTK", loaiTK);
+                    mycon.cmd.Parameters.AddWithValue("@idNV", FrmInfoNhanVien.idNV);
+
+                    mycon.cmd.ExecuteNonQuery();
+                    MessageBox.Show("Thêm tài khoản thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            this.Close();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            // Cập nhật thông tin tài khoản
+            SQLCONNECTION mycon = new SQLCONNECTION();
+            mycon.conn.Open();
+            // Lúc chọn radio button có giá trị là Admin thì sẽ lưu 1, Nhân viên thì giá trị lưu sẽ là 2
+            int loaiTK = 0;
+            if (RbAdmin.Checked == true)
+            {
+                loaiTK = 1;
+            }
+            else if (RbNV.Checked == true)
+            {
+                loaiTK = 2;
+            }
+            // Đảm bảo rằng giá trị idNV là một chuỗi ký tự trước khi sử dụng nó trong câu lệnh SQL
+            //Kiểm tra các thông tin đã được nhập hết chưa
+            if (TxtUsername.Text == "" || TxtPassword.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                // Cập nhật thông tin tài khoản
+                string sqlUpdate = "UPDATE TaiKhoan SET UserName = @UserName, Pass = @Pass, LoaiTK = @LoaiTK WHERE idNV = @idNV";
+                mycon.cmd = new SqlCommand(sqlUpdate, mycon.conn);
+                mycon.cmd.Parameters.AddWithValue("@UserName", TxtUsername.Text);
+                mycon.cmd.Parameters.AddWithValue("@Pass", TxtPassword.Text);
+                mycon.cmd.Parameters.AddWithValue("@LoaiTK", loaiTK);
+                mycon.cmd.Parameters.AddWithValue("@idNV", FrmInfoNhanVien.idNV);
+
+                mycon.cmd.ExecuteNonQuery();
+                MessageBox.Show("Cập nhật tài khoản thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            this.Close();
+        }
+    }
+}
