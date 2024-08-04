6de2213e5a6fa32efd6ce26569aedc6a40f3604d
ka do doka <hoangkavudo@gmail.com>
2024-08-04 23:29:55 +0700

    Add project files.

---
+++ (Final_Project)Cinema_Theater/SQLCONNECTION.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _Final_Project_Cinema_Theater
+{
+    internal class SQLCONNECTION
+    {
+        // Chuỗi kết nối
+        private string connectionString;
+
+        // Đối tượng kết nối
+        public SqlConnection conn;
+        public SqlCommand cmd;
+        public SqlDataReader dta;
+        public DataTable dt;
+
+        public SQLCONNECTION()
+        {
+            // Khởi tạo chuỗi kết nối
+            connectionString = "Data Source=DESKTOP-3TRF427\\HOANGKA;Initial Catalog=QLRapPhim;Integrated Security=True";
+
+            // Khởi tạo đối tượng kết nối
+            conn = new SqlConnection(connectionString);
+        }
+
+        // Trả về đối tượng kết nối
+        public static SqlConnection GetConnection()
+        {
+            SQLCONNECTION sqlConnection = new SQLCONNECTION();
+            return sqlConnection.conn;
+        }
+    }
+}
---
+++ (Final_Project)Cinema_Theater/vefirm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Final_Project_Cinema_Theater
+{
+    internal class vefirm
+    {
+        string IDVE;
+
+
+        public string idGhe { get; set; }
+        public string TongTien { get; set; }
+
+
+
+        public vefirm(string term)
+        {
+            this.IDVE = term;
+        }
+      
+
+        public vefirm( string idGhe,string tongtien)
+        {
+
+            idGhe = idGhe;
+            TongTien = tongtien;
+
+        }
+
+        string chuoiKetNoi = @"Data Source=DESKTOP-3TRF427\HOANGKA;Initial Catalog=QLRapPhim;Integrated Security=True";
+
+
+
+        public List<vefirm> Getvefirm(string idhd)
+        {
+            using (SqlConnection connection = new SqlConnection(chuoiKetNoi))
+            {
+                connection.Open();
+
+                string sqlkh = $"SELECT MaGheNgoi,TienBanVe FROM VE WHERE idVe = '{37}'";
+
+                using (SqlCommand command = new SqlCommand(sqlkh, connection))
+                {
+                    command.Parameters.AddWithValue("@IDHD", idhd);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<vefirm> list = new List<vefirm>();
+                        while (reader.Read())
+                        {
+                            list.Add(new vefirm(
+
+                                reader.GetString(0),
+                                reader.GetString(1)
+
+                            ));
+                        }
+                        return list;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
